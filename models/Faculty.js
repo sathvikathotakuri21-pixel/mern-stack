830861329cb45a754c8917fc4ace3fde33849951
sathvikathotakuri21-pixel <sathvikathotakuri21@gmail.com>
2026-03-13 18:04:18 +0530

    faculty

---
+++ models/Faculty.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const FacultySchema = new mongoose.Schema({
+    name: String,
+    designation: String,
+    salary: Number
+});
+
+module.exports = mongoose.model("Faculty",
+    FacultySchema);
